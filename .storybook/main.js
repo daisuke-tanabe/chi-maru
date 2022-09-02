85e066754ad61a888d0d2410805a5aa8ccbea81b
daisuke-tanabe <helloworld0840@gmail.com>
2022-09-02 20:40:39 +0900

    refactor: storiesディレクトリを削除したので関連記述を削除する

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,6 @@
 module.exports = {
   stories: [
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   features: {
