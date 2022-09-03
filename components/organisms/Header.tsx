aba4811d3e3906ebf23f0f53289811f1ca0930ad
daisuke-tanabe <helloworld0840@gmail.com>
2022-09-03 21:34:01 +0900

    change: サイトタイトルをリンクにする

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,12 +1,29 @@
+import React from "react";
+import Link from "next/link";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import MuiLink from "@mui/material/Link"
 
 const Header = () => {
   return (
     <header>
       <Container maxWidth="md" sx={{ textAlign: "center" }}>
-        <Typography variant="h4" component="h1" py={18}>ち。</Typography>
+        <Typography variant="h4" component="h1" py={18}>
+          <Link
+            href="/"
+            prefetch={false}
+            passHref
+          >
+            <MuiLink
+              underline="none"
+              css={{
+                color: '#333'
+              }}
+            >
+              ち。
+            </MuiLink>
+          </Link>
+        </Typography>
       </Container>
     </header>
   );
